d3cb7cfe8c5bc5042b6e25812fd75c01e3ed7380
legeriergeek <102820839+legeriergeek@users.noreply.github.com>
2023-01-24 17:09:24 +0100

    Update abos.py

--- abos.py
+++ abos.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 # Replace YOUR_API_KEY with your actual API key
-api_key = "AIzaSyBfbd6ScZnfRfZSm-HBjg78gF5SEq7G2m8"
+api_key = "YOUR_API_KEY"
 
 # Replace CHANNEL_ID with the ID of the channel you want to check
-channel_id = "UCidmlliiEyQjaiKGZ69F3GQ"
+channel_id = "CHANNEL_ID"
 
 # Make a GET request to the YouTube Data API
 url = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={api_key}"
